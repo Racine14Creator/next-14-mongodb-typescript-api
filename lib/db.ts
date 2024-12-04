aa27a60c5a7ecd9d24120bb1b708c58ba561e9df
racine14creator <bisimwagrace596@gmail.com>
2024-12-04 12:20:37 +0200

    Mongodb Connected to Atlas

---
+++ lib/db.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+const connect = async () => {
+  const connectionState = mongoose.connection.readyState;
+
+  // Pour verifier la connexion
+  if (connectionState === 1) {
+    console.log("Deja connecté");
+    return;
+  }
+
+  if (connectionState === 2) {
+    console.log("Connecting...");
+    return;
+  }
+
+  try {
+    mongoose.connect(MONGO_URI!, {
+      dbName: "rest-api-mongo-protect",
+      bufferCommands: true,
+    });
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
