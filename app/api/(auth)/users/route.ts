7278099f824f822ce684b2b2819c344cb6973d6e
racine14creator <bisimwagrace596@gmail.com>
2024-12-04 13:00:46 +0200

    Delete user implemented

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -23,7 +23,21 @@ export const POST = async (req: Request) => {
     const user = new User({ name, email, password });
     await user.save();
 
-    return new NextResponse("User created successfully", { status: 201 });
+    return new NextResponse(
+      JSON.stringify({ message: "User created successfully", user }),
+      { status: 201 }
+    );
+  } catch (error) {
+    return new NextResponse("Database connection failed", { status: 500 });
+  }
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    await connect();
+    const { id } = await req.json();
+    await User.findByIdAndDelete(id);
+    return new NextResponse("User deleted successfully", { status: 200 });
   } catch (error) {
     return new NextResponse("Database connection failed", { status: 500 });
   }
